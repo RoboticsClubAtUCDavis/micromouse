48050d0fb6ea71ea91f6dbdfcad720c804cb4267
Ian Douglas Scott <ian@iandouglasscott.com>
2017-02-09 11:36:17 -0800

    Initial Node implementation

---
+++ Node.cpp
@@ -0,0 +1,13 @@
+#include "Node.h"
+
+Node::Node(void) {
+    exists = true;
+}
+
+Node::operator bool() const {
+    return exists;
+}
+
+void Node::remove(void) {
+    exists = false;
+}
---
+++ Node.h
@@ -0,0 +1,8 @@
+class Node {
+    public:
+        Node(void);
+        explicit operator bool() const;
+        void remove(void);
+    private:
+        bool exists;
+};
