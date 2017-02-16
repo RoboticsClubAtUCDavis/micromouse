da3272a3ccd6312cefdc9585006291d160b6fb4a
Ian Douglas Scott <ian@iandouglasscott.com>
2017-02-15 21:20:08 -0800

    Run clang-format

--- Direction.h
+++ Direction.h
@@ -1,12 +1,11 @@
 #pragma once
 
-enum Direction { N, W, E, S, NE, NW, SW, SE};
+enum Direction { N, W, E, S, NE, NW, SW, SE };
 
 class DirectionVector {
-	public:
-		DirectionVector();
-		DirectionVector(Direction direction, int magnitude);
-		int magnitude;
-		Direction direction;
-
+  public:
+    DirectionVector();
+    DirectionVector(Direction direction, int magnitude);
+    int magnitude;
+    Direction direction;
 };
--- Maze.h
+++ Maze.h
@@ -1,23 +1,24 @@
 #ifndef MAZE_H
 #define MAZE_H
 
-#include "Node.h"
 #include "Coordinate.h"
 #include "Direction.h"
+#include "Node.h"
 
 const int ROWS = 31;
 const int COLS = 31;
 
 class Maze {
-	public:
-		Maze();
-		Maze(int rows, int cols);
-		bool isWall(Coordinate pos);
-		bool isWall(Coordinate pos, Direction dir);
-		void setWall(Coordinate pos, Direction dir, bool wall);
-		void reset();
-	private:
-		Node maze[ROWS][COLS];
+  public:
+    Maze();
+    Maze(int rows, int cols);
+    bool isWall(Coordinate pos);
+    bool isWall(Coordinate pos, Direction dir);
+    void setWall(Coordinate pos, Direction dir, bool wall);
+    void reset();
+
+  private:
+    Node maze[ROWS][COLS];
 };
 
 #endif
