0f652e264b528c0700f7084210f6ed95c5d1988e
daniel9a <andrade.daniel111@gmail.com>
2017-02-15 20:02:21 -0800

    Added coordinate header and direction header

---
+++ Coordinate.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Coordinate {
+	public:
+		Coordinate();
+		Coordinate(int x, int y);
+		int x;
+		int y;
+};
---
+++ Direction.h
@@ -0,0 +1,12 @@
+#pragma once
+
+enum Direction { N, W, E, S, NE, NW, SW, SE};
+
+class DirectionVector {
+	public:
+		DirectionVector();
+		DirectionVector(Direction direction, int magnitude);
+		int magnitude;
+		Direction direction;
+
+};
