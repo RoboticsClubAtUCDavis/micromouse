9b7bb0729cd8a3e0a3df2229fc456ee74068c18e
Ian Douglas Scott <ian@iandouglasscott.com>
2017-02-24 20:48:21 -0800

    Draw border in simulator

--- simulate.cpp
+++ simulate.cpp
@@ -1,21 +1,57 @@
 #include <SFML/Graphics.hpp>
 
+#include "Coordinate.h"
 #include "Maze.h"
+
+const int CELL_SIZE = 30;
+const int WINDOW_WIDTH = 10 + Maze::CELL_COLS * CELL_SIZE;
+const int WINDOW_HEIGHT = 10 + Maze::CELL_ROWS * CELL_SIZE;
+const int MARGIN = 5;
+const std::string WINDOW_TITLE = "Micromouse simulator";
 
 class Simulator {
   public:
-    Simulator(void) : maze() {
+    Simulator(sf::RenderWindow &window) : window(window), maze() {
     }
-    void draw(sf::RenderWindow &window) {
+
+    void drawBorder(void) {
+        const sf::Vertex border[] = {
+            sf::Vertex(sf::Vector2f(MARGIN, MARGIN)),
+            sf::Vertex(sf::Vector2f(WINDOW_WIDTH - MARGIN, MARGIN)),
+            sf::Vertex(
+                sf::Vector2f(WINDOW_WIDTH - MARGIN, WINDOW_HEIGHT - MARGIN)),
+            sf::Vertex(sf::Vector2f(MARGIN, WINDOW_HEIGHT - MARGIN)),
+            sf::Vertex(sf::Vector2f(MARGIN, MARGIN)),
+        };
+
+        window.draw(border, 5, sf::LineStrip);
+    }
+
+    void draw(void) {
+        window.clear(sf::Color::Black);
+
+        for (int row = 0; row != Maze::NODE_ROWS; row++) {
+            for (int col = 0; col != Maze::NODE_COLS; col++) {
+                if (maze.isWall(NodeCoordinate(row, col))) {
+                }
+            }
+        }
+
+        drawBorder();
+
+        window.display();
     }
 
   private:
     Maze maze;
+    sf::RenderWindow &window;
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Micromouse simulator");
-    Simulator simulator;
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+                            WINDOW_TITLE,
+                            sf::Style::Titlebar | sf::Style::Close);
+    Simulator simulator(window);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -24,9 +60,7 @@ int main() {
                 window.close();
         }
 
-        window.clear(sf::Color::Black);
-        simulator.draw(window);
-        window.display();
+        simulator.draw();
     }
 
     return 0;
