ebd55f14fc8daadca0655d66b3f3a2ac42a8e65a
blhough <benhuff322@gmail.com>
2017-02-26 21:42:17 -0800

    Fix x and y swapped on maze access

--- Maze.cpp
+++ Maze.cpp
@@ -5,15 +5,15 @@ const CellCoordinate Maze::CELL_START = CellCoordinate(0,0);
 const CellCoordinate  Maze::CELL_FINISH = CellCoordinate(Maze::CELL_COLS / 2, (Maze::CELL_ROWS / 2);
 
 void Maze::reset() {
-    for (int i = 0; i < NODE_ROWS; i++)
-        for (int j = 0; i < NODE_COLS; j++)
-            maze[i][j] = Node();
+	for (int y = 0; y < NODE_ROWS; y++)
+		for (int x = 0; x < NODE_COLS; x++)
+			maze[y][x] = Node(NodeCoordinate(x, y));
 }
 
 
 
 bool Maze::isWall(NodeCoordinate pos) {
-    return !maze[pos.x][pos.y];
+    return !maze[pos.y][pos.x];
 }
 
 bool Maze::isWall(CellCoordinate pos, Direction dir) {
@@ -37,11 +37,11 @@ bool Maze::isWall(CellCoordinate pos, Direction dir) {
             throw std::invalid_argument("Direction must be: N, S, E, W");
     }
 
-    return !maze[x][y];
+    return !maze[y][x];
 }
 
 void Maze::setWall(NodeCoordinate pos, bool wall) {
-    maze[pos.x][pos.y].exists = !wall;
+    maze[pos.y][pos.x].exists = !wall;
 }
 
 void Maze::setWall(CellCoordinate pos, Direction dir, bool wall) {
@@ -65,7 +65,7 @@ void Maze::setWall(CellCoordinate pos, Direction dir, bool wall) {
             throw std::invalid_argument("Direction must be: N, S, E, W");
     }
 
-    maze[x][y].exists = !wall;
+    maze[y][x].exists = !wall;
 }
 
 
