dcf2b617b9a1cb13876d5bec564ea51ec4820848
daniel9a <andrade.daniel111@gmail.com>
2017-03-01 23:43:00 -0800

    Created empty classes for motor, encoder, rangefinder, led, and hardware

---
+++ Hardware.cpp
@@ -0,0 +1,23 @@
+#include "Hardware.h"
+#include "Motor.h"
+#include "RangeFinder.h"
+#include "Encoder.h"
+#include "Led.h"
+
+class Hardware
+{
+public:
+	Hardware();
+	~Hardware();
+
+private:
+
+};
+
+Hardware::Hardware()
+{
+}
+
+Hardware::~Hardware()
+{
+}
