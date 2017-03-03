68c810982a4ea5eab74cb56d2801c207d20f0b06
Ian Douglas Scott <ian@iandouglasscott.com>
2017-03-03 14:04:50 -0800

    Create MazeDrawable class, and move some logic to it

--- simulate.cpp
+++ simulate.cpp
@@ -12,6 +12,74 @@
 const int MARGIN = 5;
 const std::string WINDOW_TITLE = "Micromouse simulator";
 
+class MazeDrawable : public sf::Drawable {
+  public:
+    MazeDrawable(Maze &maze, int node_size) : maze(maze), node_size(node_size) {}
+  private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        for (int row = 0; row < Maze::CELL_ROWS; row++) {
+            for (int col = 0; col < Maze::CELL_COLS; col++) {
+                drawCell(target, states, CellCoordinate(col, row));
+            }
+        }
+
+        drawPath(target, states, maze.getPath());
+    }
+
+    sf::Vertex nodeVertex(NodeCoordinate c, sf::Color color) const {
+        int x_p = MARGIN + c.x * node_size;
+        int y_p = MARGIN + (Maze::NODE_ROWS - c.y) * node_size;
+        return sf::Vertex(sf::Vector2f(x_p, y_p), color);
+    }
+
+    void drawLine(sf::RenderTarget& target, sf::RenderStates states, NodeCoordinate c1, NodeCoordinate c2,
+                  sf::Color color = sf::Color::White) const {
+        sf::Vertex line[] = {nodeVertex(c1, color), nodeVertex(c2, color)};
+        target.draw(line, 2, sf::Lines, states);
+    }
+
+    void drawCell(sf::RenderTarget& target, sf::RenderStates states, CellCoordinate pos) const {
+        NodeCoordinate node = pos;
+        if (maze.isWall(pos, N)) {
+            drawLine(target, states, node + NW, node + NE);
+        }
+
+        if (maze.isWall(pos, S)) {
+            drawLine(target, states, node + SW, node + SE);
+        }
+
+        if (maze.isWall(pos, E)) {
+            drawLine(target, states, node + SE, node + NE);
+        }
+
+        if (maze.isWall(pos, W)) {
+            drawLine(target, states, node + SW, node + NW);
+        }
+
+        sf::RectangleShape cell(
+            sf::Vector2f(node_size * 2 - 1, node_size * 2 - 1));
+        cell.move(sf::Vector2f(MARGIN + (node.x - 1) * node_size,
+                               MARGIN + (Maze::NODE_ROWS - (node.y + 1)) *
+                                            node_size));
+        cell.setFillColor(maze.getNode(node).evaluated
+                              ? sf::Color(255, 255, 150, 20)
+                              : sf::Color(255, 255, 255, 10));
+        target.draw(cell, states);
+    }
+
+    void drawPath(sf::RenderTarget& target, sf::RenderStates states, const Path &path) const {
+        NodeCoordinate node = path.start;
+        for (auto &i : path) {
+            NodeCoordinate nextNode = node + i;
+            drawLine(target, states, node, nextNode, sf::Color::Green);
+            node = nextNode;
+        }
+    }
+
+    Maze& maze;
+    int node_size;
+};
+
 class Simulator : public sf::RenderWindow {
   public:
     Simulator(void)
@@ -61,67 +129,10 @@ class Simulator : public sf::RenderWindow {
         node_size = std::min(x, y);
     }
 
-    sf::Vertex nodeVertex(NodeCoordinate c, sf::Color color) {
-        int x_p = MARGIN + c.x * node_size;
-        int y_p = MARGIN + (Maze::NODE_ROWS - c.y) * node_size;
-        return sf::Vertex(sf::Vector2f(x_p, y_p), color);
-    }
-
-    void drawLine(NodeCoordinate c1, NodeCoordinate c2,
-                  sf::Color color = sf::Color::White) {
-        sf::Vertex line[] = {nodeVertex(c1, color), nodeVertex(c2, color)};
-        draw(line, 2, sf::Lines);
-    }
-
-    void drawCell(CellCoordinate pos) {
-        NodeCoordinate node = pos;
-        if (maze.isWall(pos, N)) {
-            drawLine(node + NW, node + NE);
-        }
-
-        if (maze.isWall(pos, S)) {
-            drawLine(node + SW, node + SE);
-        }
-
-        if (maze.isWall(pos, E)) {
-            drawLine(node + SE, node + NE);
-        }
-
-        if (maze.isWall(pos, W)) {
-            drawLine(node + SW, node + NW);
-        }
-
-        sf::RectangleShape cell(
-            sf::Vector2f(node_size * 2 - 1, node_size * 2 - 1));
-        cell.move(sf::Vector2f(MARGIN + (node.x - 1) * node_size,
-                               MARGIN + (Maze::NODE_ROWS - (node.y + 1)) *
-                                            node_size));
-        cell.setFillColor(maze.getNode(node).evaluated
-                              ? sf::Color(255, 255, 150, 20)
-                              : sf::Color(255, 255, 255, 10));
-        draw(cell);
-    }
-
-    void drawPath(const Path &path) {
-        NodeCoordinate node = path.start;
-        for (auto &i : path) {
-            NodeCoordinate nextNode = node + i;
-            drawLine(node, nextNode, sf::Color::Green);
-            node = nextNode;
-        }
-    }
 
     void render(void) {
         clear(sf::Color::Black);
-
-        for (int row = 0; row < Maze::CELL_ROWS; row++) {
-            for (int col = 0; col < Maze::CELL_COLS; col++) {
-                drawCell(CellCoordinate(col, row));
-            }
-        }
-
-        drawPath(maze.getPath());
-
+        draw(MazeDrawable(maze, node_size));
         display();
     }
 
