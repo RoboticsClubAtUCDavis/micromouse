625742601a9c425e92e07218994972127639bd48
blhough <benhuff322@gmail.com>
2017-03-05 16:21:55 -0800

    Add function to invert Directions and DirectionVectors

--- Direction.h
+++ Direction.h
@@ -2,10 +2,15 @@
 
 enum Direction { N, NE, E, SE, S, SW, W, NW, NONE };
 
+namespace DirOp {
+Direction invert(Direction direction);
+}
+
 class DirectionVector {
   public:
     DirectionVector();
     DirectionVector(Direction direction, int magnitude);
+    DirectionVector invert() const;
     int magnitude;
     Direction direction;
 };
