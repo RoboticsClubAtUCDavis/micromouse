c5c975eb0236daf92d427fedb9e574e8e8282957
blhough <benhuff322@gmail.com>
2017-03-05 17:48:51 -0800

    Add Mouse class

---
+++ Mouse.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Coordinate.h"
+#include "Direction.h"
+#include "Hardware.h"
+#include "Maze.h"
+#include "Path.h"
+
+class Mouse {
+  public:
+    Mouse();
+
+    void mapMaze();
+    void runMaze();
+
+    // In millimeter per second.
+    void setMappingSpeed(unsigned mmps);
+    void setRunningSpeed(unsigned mmps);
+
+    void reset();
+
+  private:
+    void followPath(const Path &path);
+    void move(DirectionVector movement);
+
+    Maze maze;
+    Hardware bot;
+
+    Coordinate position;
+    Direction facing;
+
+    // In millimeter per second.
+    int speed = 100;
+};
