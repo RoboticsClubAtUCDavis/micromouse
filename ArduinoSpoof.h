8fc87ab8c9693a205b19bebde0cd752bb21f2a9e
blhough <benhuff322@gmail.com>
2017-03-09 13:53:00 -0800

    Fix unused parameters

--- ArduinoSpoof.h
+++ ArduinoSpoof.h
@@ -15,35 +15,40 @@
 #define OUTPUT_OPENDRAIN 4
 #define INPUT_DISABLE 5
 
-#define VERBOSE_ARDUINO
-
-#ifdef VERBOSE_ARDUINO
-#define verbose_print(str) std::cout << str << std::endl;
-#else
-#define verbose_print(str)
-#endif // VERBOSE_ARDUINO
+//#define VERBOSE_ARDUINO
 
 void pinMode(uint8_t pin, uint8_t mode) {
 #ifdef VERBOSE_ARDUINO
     printf("pinMode - pin: %u, mode: %u\n", pin, mode);
+#else
+    (void)pin;
+    (void)mode;
 #endif // VERBOSE_ARDUINO
 }
 
 void digitalWrite(uint8_t pin, uint8_t val) {
 #ifdef VERBOSE_ARDUINO
     printf("digitalWrite - pin: %u, val: %u\n", pin, val);
+#else
+    (void)pin;
+    (void)val;
 #endif // VERBOSE_ARDUINO
 }
 
 void analogWrite(uint8_t pin, int val) {
 #ifdef VERBOSE_ARDUINO
     printf("analogWrite - pin: %u, val: %i\n", pin, val);
+#else
+    (void)pin;
+    (void)val;
 #endif // VERBOSE_ARDUINO
 }
 
 uint8_t digitalRead(uint8_t pin) {
 #ifdef VERBOSE_ARDUINO
     printf("digitalRead - pin: %u\n", pin);
+#else
+    (void)pin;
 #endif // VERBOSE_ARDUINO
     return 0;
 }
@@ -51,6 +56,8 @@ uint8_t digitalRead(uint8_t pin) {
 int analogRead(uint8_t pin) {
 #ifdef VERBOSE_ARDUINO
     printf("analogRead - pin: %u\n", pin);
+#else
+    (void)pin;
 #endif // VERBOSE_ARDUINO
     return 0;
 }
@@ -72,12 +79,16 @@ uint32_t micros(void) {
 void delay(uint32_t msec) {
 #ifdef VERBOSE_ARDUINO
     printf("delay - msec: %u\n", msec);
+#else
+    (void)msec;
 #endif // VERBOSE_ARDUINO
 }
 
 void delayMicroseconds(uint32_t usec) {
 #ifdef VERBOSE_ARDUINO
     printf("delayMicroseconds - pusecin: %u\n", usec);
+#else
+    (void)usec;
 #endif // VERBOSE_ARDUINO
 }
 
