cd684d3514106694ff4305ada6a611d07f6fbb84
blhough <benhuff322@gmail.com>
2017-03-09 15:06:10 -0800

    Update RangeFinder to compile

--- Micromouse/RangeFinder.cpp
+++ Micromouse/RangeFinder.cpp
@@ -9,8 +9,26 @@ RangeFinder::RangeFinder(unsigned pin, unsigned minRange, unsigned maxRange)
     : DATA_PIN(pin), MIN_RANGE(minRange), MAX_RANGE(maxRange) {
 
     if (DATA_PIN == 0) {
-        throw std::invalid_argument("Pin value not set!");
+        // So Teensy/Arduino doesn't support exceptions
+        // We should implement some way to log errors
+        // throw std::invalid_argument("Pin value not set!");
+        std::cout << "Warning: Pin not set" << std::endl;
+        return;
     }
 
     pinMode(DATA_PIN, INPUT);
+}
+
+bool RangeFinder::calibrate() {
+    // TODO
+    return false;
+}
+
+bool RangeFinder::loadCalibration() {
+    // TODO
+    return false;
+}
+
+void RangeFinder::saveCalibration() {
+    // TODO
 }
