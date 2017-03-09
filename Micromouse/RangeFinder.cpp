ae0c1da05534a706c443ae14fc6fd85fd5b9f563
blhough <benhuff322@gmail.com>
2017-03-09 13:22:55 -0800

    Use ArduinoSpoof in RangeFinder

--- Micromouse/RangeFinder.cpp
+++ Micromouse/RangeFinder.cpp
@@ -1,4 +1,5 @@
 #include "RangeFinder.h"
+#include "ArduinoSpoof.h"
 #include <stdexcept>
 
 RangeFinder::RangeFinder() : DATA_PIN(0), MIN_RANGE(0), MAX_RANGE(0) {
@@ -6,9 +7,10 @@ RangeFinder::RangeFinder() : DATA_PIN(0), MIN_RANGE(0), MAX_RANGE(0) {
 
 RangeFinder::RangeFinder(unsigned pin, unsigned minRange, unsigned maxRange)
     : DATA_PIN(pin), MIN_RANGE(minRange), MAX_RANGE(maxRange) {
-    // pinMode(DATA_PIN,INPUT);
-    if (DATA_PIN == 0)
-    {
+
+    if (DATA_PIN == 0) {
         throw std::invalid_argument("Pin value not set!");
     }
+
+    pinMode(DATA_PIN, INPUT);
 }
