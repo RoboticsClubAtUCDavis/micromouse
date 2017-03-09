59b16d7b5daa93f6812bf576497e32d184c088e3
daniel9a <andrade.daniel111@gmail.com>
2017-03-08 18:55:54 -0800

    Implimented Led class

--- Led.cpp
+++ Led.cpp
@@ -5,3 +5,40 @@ Led::Led() {
 
 Led::~Led() {
 }
+
+Led::Led(unsigned pin)
+{
+//	pinMode(pin, OUTPUT);
+}
+
+void Led::setBrightness(unsigned percent)
+{
+	unsigned int percent[100];
+//	analogWrite(led, precent);
+}
+
+void Led::turnOn()
+{
+//	digitalWrite(pin, HIGH);
+}
+
+void Led::turnOff()
+{
+//	digitalWrite(pin, LOW);
+}
+
+void Led::blink(unsigned n = 1, unsigned delay1 = 200, unsigned delay2 = 200)
+{
+	for (int i; i < n; i++)
+	{
+		turnOn();
+//		delay(delay1);
+		turnOff();
+		if (i == n)
+		{
+			break;
+		}
+//		delay(delay2);		
+	}
+}
+
--- Led.h
+++ Led.h
@@ -5,5 +5,12 @@ class Led {
     Led();
     ~Led();
 
+	const unsigned pin;
+	explicit Led(unsigned pin);
+	void setBrightness(unsigned percent);
+	void turnOn();
+	void turnOff();
+	void blink(unsigned n = 1, unsigned delay1 = 200, unsigned delay2 = 200);
+
   private:
 };
