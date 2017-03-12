1e4db34279a5be6794ba69975a1838051528bf80
Ian Douglas Scott <ian@iandouglasscott.com>
2017-03-12 16:40:54 -0700

    Implement Motor class

--- Micromouse/Motor.cpp
+++ Micromouse/Motor.cpp
@@ -1,7 +1,34 @@
 #include "Motor.h"
+#include <Arduino.h>
 
-Motor::Motor() {
+Motor::Motor(void) : Motor(0, 0, 0) {
+}
+
+Motor::Motor(unsigned int en, unsigned int in1, unsigned int in2)
+    : en(en), in1(in1), in2(in2) {
+    pinMode(en, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    off();
+}
+
+void Motor::off(void) {
+    digitalWrite(en, LOW);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+}
+
+void Motor::setSpeed(float speed) {
+    if (speed < 0) {
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+    } else {
+        digitalWrite(in2, LOW);
+        digitalWrite(in1, HIGH);
+    }
+    analogWrite(en, abs(speed * 255));
 }
 
 Motor::~Motor() {
+    off();
 }
