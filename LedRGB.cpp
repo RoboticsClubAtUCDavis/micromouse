e215913b21f1dfd02307dd507b3272adfb00ec55
daniel9a <andrade.daniel111@gmail.com>
2017-03-13 21:26:03 -0700

    Changed brightness to float, set default values for colors and brightness. Changed setRGB and setBrightness functions

--- LedRGB.cpp
+++ LedRGB.cpp
@@ -1,7 +1,7 @@
 #include "LedRGB.h"
 #include "Led.h"
 
-LedRGB::LedRGB() : RedLed(0), GreenLed(0), BlueLed(0) {
+LedRGB::LedRGB() : RedLed(255), GreenLed(255), BlueLed(255) {
 }
 
 LedRGB::LedRGB(unsigned redPin, unsigned greenPin, unsigned bluePin)
@@ -12,26 +12,16 @@ void LedRGB::setRGB(unsigned r, unsigned g, unsigned b) {
     redBrightness = r / 2.55;
     greenBrightness = g / 2.55;
     blueBrightness = b / 2.55;
-    RedLed.setBrightness(redBrightness);
-    GreenLed.setBrightness(greenBrightness);
-    BlueLed.setBrightness(blueBrightness);
-    setColors = true;
-    if (!checkBrightness) {
-        setBrightness(brightness);
-    }
+    RedLed.setBrightness(redBrightness * brightness);
+    GreenLed.setBrightness(greenBrightness * brightness);
+    BlueLed.setBrightness(blueBrightness * brightness);
 }
 
 void LedRGB::setBrightness(unsigned percent) {
     brightness = percent / 100;
-    redBrightness *= brightness;
-    greenBrightness *= brightness;
-    blueBrightness *= brightness;
-    if (setColors) {
-        RedLed.setBrightness(redBrightness);
-        GreenLed.setBrightness(greenBrightness);
-        BlueLed.setBrightness(blueBrightness);
-        checkBrightness = true;
-    }
+    RedLed.setBrightness(redBrightness * brightness);
+    GreenLed.setBrightness(greenBrightness * brightness);
+    BlueLed.setBrightness(blueBrightness * brightness);
 }
 
 void LedRGB::turnOn() {
