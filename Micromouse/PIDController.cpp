902672fa499ae4ab12b669ee752383bd0707b18a
blhough <benhuff322@gmail.com>
2017-03-28 22:31:00 -0700

    Add PIDController class

---
+++ Micromouse/PIDController.cpp
@@ -0,0 +1,49 @@
+#include "PIDController.h"
+#include <algorithm>
+#include <assert.h>
+
+PIDController::PIDController(float p, float i, float d, const Delta &getDelta,
+                             float initialError, float maxIntegralError,
+                             float derivativeSmoothing)
+    : P(p)
+    , I(i)
+    , D(d)
+    , getDelta(getDelta)
+    , maxIntegralError(maxIntegralError)
+    , derivativeSmoothing(derivativeSmoothing)
+    , integratedError(0.0f)
+    , previousError(initialError) {
+
+    assert(derivativeSmoothing >= 0.0f && derivativeSmoothing < 1.0f);
+    assert(P >= 0.0f);
+    assert(I >= 0.0f);
+    assert(D >= 0.0f);
+    assert(maxIntegralError > 0.0f);
+}
+
+float PIDController::getCorrection(float currentError) {
+
+    unsigned delta = getDelta();
+
+    integratedError += currentError * delta;
+
+    // Bound the integral error between the +/- maximums.
+    integratedError = std::max(integratedError, -maxIntegralError);
+    integratedError = std::min(integratedError, maxIntegralError);
+
+    float pTerm = P * currentError;
+    float iTerm = I * integratedError;
+
+    // Not sure if this is the best way to handle `delta = 0` cases.
+    float dTerm = D * ((currentError - previousError) / (delta + 1));
+
+    previousError = derivativeSmoothing * previousError +
+                    (1 - derivativeSmoothing) * currentError;
+
+    return pTerm + iTerm + dTerm;
+}
+
+void PIDController::clear(float initialError) {
+    integratedError = 0.0f;
+    previousError = initialError;
+}
---
+++ Micromouse/PIDController.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class PIDController {
+  public:
+    class Delta {
+      public:
+        Delta(){};
+        virtual unsigned operator()() const {
+            return 0;
+        };
+    };
+
+    PIDController();
+    PIDController(float p, float i, float d, const Delta &delta,
+                  float initialError, float maxIntegralError,
+                  float derivativeSmoothing = 0.0f);
+
+    float getCorrection(float currentError);
+    void clear(float initialError);
+
+  private:
+    const float P;
+    const float I;
+    const float D;
+
+    const Delta &getDelta;
+    float maxIntegralError;
+    float derivativeSmoothing;
+    float integratedError;
+    float previousError;
+};
