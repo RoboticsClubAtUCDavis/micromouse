4a6459d80f55d85365821916a229636a6bba3e21
Ian Douglas Scott <ian@iandouglasscott.com>
2017-05-12 17:58:19 -0700

    Correct motor pins

--- Micromouse/Hardware.h
+++ Micromouse/Hardware.h
@@ -18,15 +18,15 @@ class Hardware {
     static const unsigned IRSENSOR_RIGHT_PIN = 17;
     static const unsigned ULTRASONIC_PIN = 14;
 
-    static const unsigned MOTOR_LEFT_EN_PIN = 4;
-    static const unsigned MOTOR_LEFT_IN1_PIN = 8;
-    static const unsigned MOTOR_LEFT_IN2_PIN = 7;
+    static const unsigned MOTOR_LEFT_EN_PIN = 3;
+    static const unsigned MOTOR_LEFT_IN1_PIN = 7;
+    static const unsigned MOTOR_LEFT_IN2_PIN = 6;
     static const unsigned ENCODER_LEFT_A_PIN = 21;
     static const unsigned ENCODER_LEFT_B_PIN = 20;
 
-    static const unsigned MOTOR_RIGHT_EN_PIN = 3;
-    static const unsigned MOTOR_RIGHT_IN1_PIN = 6;
-    static const unsigned MOTOR_RIGHT_IN2_PIN = 5;
+    static const unsigned MOTOR_RIGHT_EN_PIN = 2;
+    static const unsigned MOTOR_RIGHT_IN1_PIN = 5;
+    static const unsigned MOTOR_RIGHT_IN2_PIN = 4;
     static const unsigned ENCODER_RIGHT_A_PIN = 23;
     static const unsigned ENCODER_RIGHT_B_PIN = 22;
 
@@ -38,7 +38,7 @@ class Hardware {
     static const unsigned BUTTON_PIN = 38;
 
     static const unsigned LED_PIN = 13;
-    // static const unsigned RGB_LED_PIN = 2; // TODO update interface
+    // static const unsigned RGB_LED_PIN = 1; // TODO update interface
 
     static const unsigned NUM_RANGE_FINDERS = 3;
     static const unsigned IRSENSOR_LEFT_MIN_DISTANCE = 20;   // mm
